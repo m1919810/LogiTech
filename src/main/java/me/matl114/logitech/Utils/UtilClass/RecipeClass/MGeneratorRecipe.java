3b1b71ed8ac94add119548026dda7a4ef25adda5
m1919810 <matl040527@qq.com>
2024-08-16 21:37:40 +0800

    feat: fix special machine drop problem

---
+++ src/main/java/me/matl114/logitech/Utils/UtilClass/RecipeClass/MGeneratorRecipe.java
@@ -0,0 +1,12 @@
+package me.matl114.logitech.Utils.UtilClass.RecipeClass;
+
+import org.bukkit.inventory.ItemStack;
+
+public class MGeneratorRecipe extends StackMachineRecipe{
+    public MGeneratorRecipe(StackMachineRecipe r){
+        super(r.getTicks(),r.getInput(),r.getOutput());
+    }
+    public MGeneratorRecipe(int ticks, ItemStack[] input, ItemStack[] output){
+        super(ticks,input,output);
+    }
+}
